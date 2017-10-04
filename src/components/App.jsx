339a5cce53e740523d535c52c6b5418cbe7eb141
sp <sampeppard@gmail.com>
2017-10-03 23:14:15 -0400

    add new components for header and ticketlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(props) {
     return (
         <div>
-            <h1>Help Queue</h1>
-            <h3>3a</h3>
-            <h3>Thato and Haley</h3>
-            <p><em>Firebase won't save record</em></p>
-            <hr/>
+            <Header/>
+            <TicketList/>
         </div>
     );
 }
